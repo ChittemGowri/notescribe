4a5395ce2946bf03ee9e7e88e55b4f1c17874d72
ChittemGowri <chittemgowrishankar@gmail.com>
2026-03-14 22:58:31 +0530

    fix: auto detect tesseract path

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, request, render_template_string, send_file, redirect
 import os
 from datetime import datetime
 
-pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+import shutil; pytesseract.pytesseract.tesseract_cmd = shutil.which('tesseract') or '/usr/bin/tesseract'
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
